ad6058e3964e1798752bc5e7b50b128df565d755
Maizels Kirill <domo.ddr@gmail.com>
2018-01-18 20:51:21 +0600

    Homework

--- Navigation/MainWindow.xaml.cs
+++ Navigation/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,70 @@ namespace Navigation
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<string> favoriteSites;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            favoriteSites = new ObservableCollection<string>
+            {
+                "https://www.google.com",
+                "https://www.yandex.com",
+                "https://www.mail.ru",
+            };
+
+            favoriteList.ItemsSource = favoriteSites;
+        }
+
+        private void UrlAddress_GotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (urlAddress.Text == "Введите URL")
+            {
+                urlAddress.Text = "";
+                urlAddress.Foreground = Brushes.Black;
+            }
+        }
+
+        private void UrlAddress_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (urlAddress.Text == "")
+            {
+                urlAddress.Text = "Введите URL";
+                urlAddress.Foreground = Brushes.Gray;
+            }
+            
+        }
+
+        private void AddToFavoriteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (urlAddress.Text.Contains("http://") || urlAddress.Text.Contains("https://"))
+            {
+                favoriteSites.Add(urlAddress.Text);
+            }
+        }
+
+        private void UrlAddress_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter&&urlAddress.Text.Contains("http://")|| urlAddress.Text.Contains("https://"))
+            {
+                browser.Navigate(urlAddress.Text);
+            }
+        }
+
+        private void FavoriteList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            browser.Navigate(favoriteList.SelectedItem.ToString());
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(urlAddress.Text);
+        }
+
+        private void PasteButton_Click(object sender, RoutedEventArgs e)
+        {
+            urlAddress.Text = Clipboard.GetText();
         }
     }
 }
